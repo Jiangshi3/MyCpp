265a4006a961385aebc9a3c6597261e8613d021d
jiang <1470475473@qq.com>
2023-09-22 21:00:45 +0800

    try catch

---
+++ day9/exception_class.cpp
@@ -0,0 +1,59 @@
+/*try catch*/
+#include <iostream>
+using namespace std;
+
+const int MAX = 3;
+class Full{};   //  堆栈满时抛出的异常类
+class Empty{};  //  堆栈空时抛出的异常类
+class Stack{
+private:
+    int top;
+    int s[MAX];
+public:
+    Stack(){
+        cout << "Stack Construct!"<<endl;
+        top=-1;};
+    void push(int x);
+    int pop();
+    void show();
+};
+void Stack::push(int x){
+    if(top>=MAX-1){
+        throw Full();
+    }
+    s[++top] = x;
+}
+int Stack::pop(){
+    if(top<0){
+        throw Empty();
+    }
+    return s[top--];
+}
+void Stack::show(){
+    for(int i=0; i<=MAX-1; ++i){
+        cout << s[i] << "\t";
+    }
+    
+}
+
+int main(int argc, const char* argv[])
+{
+    Stack s;
+    try{
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        //s.push(4); // 将产生栈满异常
+        s.show();
+        s.pop();
+        s.pop();
+        s.pop();
+        s.pop();  // 将产生栈空异常
+    }catch(Full){
+        cout << "Exception: Stack Full" << endl;
+    }catch(Empty){
+        cout << "Exception: Stack Empty" << endl;
+    }
+
+    return 0;
+}
