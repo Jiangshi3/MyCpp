1cb305b29454cb87ff45d424d467207420df869c
jiang <1470475473@qq.com>
2023-10-07 20:09:28 +0800

    返回值和返回引用的区别

--- 实战练习/function_template_spec.cpp
+++ 实战练习/function_template_spec.cpp
@@ -4,7 +4,11 @@
 using namespace std;
 
 template <class T>
-T compareMax(T a, T b){return (a>b)?a:b;}
+T compareMax(T a, T b){return (a>b)?a:b;}  //返回一个值（通过值传递），这意味着它会创建a和b的副本，并将比较结果的副本返回给调用者。这个版本适用于非修改性操作
+
+// template <class T>
+// T& compareMax(T a, T b){return (a>b)?a:b;}  //返回一个引用，这意味着它返回a或b的引用，取决于比较的结果。适用于需要修改原始值的情况，因为它可以直接修改传递给函数的参数
+// 如果你只需要比较两个值并返回比较结果，第一种写法足够了
 
 template<>
 const char* compareMax(const char* s1, const char* s2){
